07ca409bd7c28b203ad94185a98f5a41edea0201
Mohammad <myarali1345@gmail.com>
2023-11-07 11:23:55 +0330

    feat: added new user promise function logic

--- src/layout/Card.tsx
+++ src/layout/Card.tsx
@@ -1,12 +1,8 @@
 import { useContext } from "react";
 import { Outlet, useLocation, useNavigate } from "react-router-dom";
 import { ErrorContext } from "../context/InputErrorContext";
-import {
-  userSchemaPartOne,
-  userSchemaPartTwo,
-} from "../helpers/userValidationSchema";
 import { UserContext } from "../context/UserContext";
-import { toast } from 'react-hot-toast';
+import addUser from "../helpers/addUserFunction";
 
 export default function Card() {
   const { pathname } = useLocation();
@@ -16,44 +12,11 @@ export default function Card() {
   const { userModelState } = useContext(UserContext);
 
   // submit function that has to be promise
-  const submitUser = () => {
-    if (pathname === "/") {
-      // Validation for step 1
-      userSchemaPartOne
-        .validate(userModelState, { abortEarly: false })
-        .then(() => {
-          navigate("/2"); // step one completed then navigates user to next step
-          setInputErrorState([{ message:"", path: "" }]) // clears error state 
-          toast.success("Step 1 completed ✨") // shows success toast
-        })
-        .catch((errors) => {
-          const err = errors.inner.map(
-            (error: { message: string; path: string }) => {
-              return { message: error.message, path: error.path };
-            }
-          ); // returns errors for each input
-          setInputErrorState(err); // sets error state
-          toast.error("Validation error") // shows error toast
-        });
-    } else if (pathname === "/2") {
-      // Validation for step 2
-      userSchemaPartTwo
-        .validate(userModelState, { abortEarly: false })
-        .then((validData) => {
-          // Proceed with submission for step 2
-          setInputErrorState([{ message:"", path: "" }]);
-          toast.success(`Account created, ${validData.email} ✨`)
-        })
-        .catch((errors) => {
-          const err = errors.inner.map(
-            (error: { message: string; path: string }) => {
-              return { message: error.message, path: error.path };
-            }
-          );
-          setInputErrorState(err);
-        });
-    }
+  const submitUser = async () => {
+    const user = await addUser(userModelState, pathname, navigate, setInputErrorState);
+    console.log(user) // do whatever with newly created user
   };
+
   return (
     <>
       <div className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-md">
